a79266809d13734dfe9c4c46e13d608b71c7a79c
romerotfe-maker <romero.tfe@gmail.com>
2026-08-18 19:41:20 +0100

    Add files via upload

---
+++ exactStats.js
@@ -0,0 +1,123 @@
+module.exports = {
+  "aeromarc": {
+    "exactInitialSquadDay1": 94383784,
+    "exactStartingCash": 205616216,
+    "totalBuys": 488865902,
+    "totalSales": 328687010,
+    "tradingProfit": 5522503,
+    "exactCalculatedCash": 45437324,
+    "squadVal": 296995305,
+    "netWorth": 346802310,
+    "playersCount": 16
+  },
+  "Borja ✈️🎧⚽️⛳️": {
+    "exactInitialSquadDay1": 92655283,
+    "exactStartingCash": 207344717,
+    "totalBuys": 711504291,
+    "totalSales": 598508458,
+    "tradingProfit": 13062629,
+    "exactCalculatedCash": 94348884,
+    "squadVal": 286535241,
+    "netWorth": 341739788,
+    "playersCount": 13
+  },
+  "ivanabyan83": {
+    "exactInitialSquadDay1": 100236262,
+    "exactStartingCash": 199763738,
+    "totalBuys": 560989999,
+    "totalSales": 356618038,
+    "tradingProfit": -21212653,
+    "exactCalculatedCash": -4608223,
+    "squadVal": 263525346,
+    "netWorth": 264235185,
+    "playersCount": 12
+  },
+  "gracioguemes": {
+    "exactInitialSquadDay1": 98667637,
+    "exactStartingCash": 201332363,
+    "totalBuys": 411689993,
+    "totalSales": 212236161,
+    "tradingProfit": -6900627,
+    "exactCalculatedCash": 1878531,
+    "squadVal": 274575633,
+    "netWorth": 267268371,
+    "playersCount": 14
+  },
+  "Mario Toledano": {
+    "exactInitialSquadDay1": 99137681,
+    "exactStartingCash": 200862319,
+    "totalBuys": 265482824,
+    "totalSales": 64784629,
+    "tradingProfit": 0,
+    "exactCalculatedCash": 164124,
+    "squadVal": 297153386,
+    "netWorth": 300337605,
+    "playersCount": 15
+  },
+  "Carlos Romero": {
+    "exactInitialSquadDay1": 101312683,
+    "exactStartingCash": 198687317,
+    "totalBuys": 341744131,
+    "totalSales": 170967438,
+    "tradingProfit": 1746916,
+    "exactCalculatedCash": 27910624,
+    "squadVal": 254738738,
+    "netWorth": 283489104,
+    "playersCount": 19
+  },
+  "Juan Ramon": {
+    "exactInitialSquadDay1": 97387139,
+    "exactStartingCash": 202612861,
+    "totalBuys": 193062101,
+    "totalSales": 0,
+    "tradingProfit": 0,
+    "exactCalculatedCash": 9550760,
+    "squadVal": 276485407,
+    "netWorth": 309757815,
+    "playersCount": 20
+  },
+  "Arturo Muradás": {
+    "exactInitialSquadDay1": 100621274,
+    "exactStartingCash": 199378726,
+    "totalBuys": 394775000,
+    "totalSales": 194266923,
+    "tradingProfit": 3084683,
+    "exactCalculatedCash": -1129351,
+    "squadVal": 283123892,
+    "netWorth": 283392748,
+    "playersCount": 14
+  },
+  "Capitan Maple": {
+    "exactInitialSquadDay1": 108097026,
+    "exactStartingCash": 191902974,
+    "totalBuys": 222523295,
+    "totalSales": 16255676,
+    "tradingProfit": 0,
+    "exactCalculatedCash": -14364645,
+    "squadVal": 202807764,
+    "netWorth": 206209440,
+    "playersCount": 15
+  },
+  "Rivers FC": {
+    "exactInitialSquadDay1": 95269664,
+    "exactStartingCash": 204730336,
+    "totalBuys": 157116641,
+    "totalSales": 2211014,
+    "tradingProfit": 0,
+    "exactCalculatedCash": 49824709,
+    "squadVal": 227454810,
+    "netWorth": 284466687,
+    "playersCount": 18
+  },
+  "jeromuradas": {
+    "exactInitialSquadDay1": 98805881,
+    "exactStartingCash": 201194119,
+    "totalBuys": 139094786,
+    "totalSales": 33832586,
+    "tradingProfit": 0,
+    "exactCalculatedCash": 95931919,
+    "squadVal": 179424161,
+    "netWorth": 277070058,
+    "playersCount": 15
+  }
+};
